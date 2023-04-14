5506aac8030bfb84029a768ea3c73cdefad09db3
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-14 22:46:35 +0800

    fix: export type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './local-storage.js'
 export * from './session-storage.js'
 export * from './web-storage.js'
-export * from './types.js'
+export type { StorageOptions } from './types.js'
